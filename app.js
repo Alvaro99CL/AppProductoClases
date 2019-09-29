b315af413f94c9d9359796dec15e84324049ae4f
Alvaro Gonzalez <agonzalezcl@gmail.com>
2019-09-29 13:04:18 -0300

    se agrega app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+//Logica de la App para agregar Producto
+//clase Producto
+class Producto {
+    constructor(nombre, precio, year) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.year = year;
+    }
+}
+;
+
+//Clase UI para modificar el dom.
+class UI {
+    addProucto(producto) {
+        //buscar elemento del dom
+    };
+
+    delProducto(elemento) {
+       //buscar elemento del dom 
+    };
+}
+
