15edd2f3993376c82c840882c9029935ad9e53e2
Alvaro Gonzalez <agonzalezcl@gmail.com>
2019-09-29 17:28:47 -0300

    agregar evento submit

--- app.js
+++ app.js
@@ -11,12 +11,38 @@ class Producto {
 
 //Clase UI para modificar el dom.
 class UI {
+
+    //METODOS
     addProucto(producto) {
         //buscar elemento del dom
+        const elemento = document.createElement('div');
+
+        //crear string con las etiquetas
+        elemento.innerHTML = `
+        <div class="card text-center mb-4">
+            <div class ="card-body">
+                <strong>Producto Nombre </strong>:${producto.nombre}
+                <strong>Precio </strong>:${producto.precio}
+                <strong>Ano </strong>:${producto.year}
+            </div>
+        </div>
+        `;
+
+        const productList = document.getElementById('product-list');
+        productList.appendChild(elemento);
     };
 
     delProducto(elemento) {
-       //buscar elemento del dom 
+        //buscar elemento del dom 
     };
 }
+
+//enventos del dom
+document.getElementById('product-form')
+    .addEventListener('submit', function (e) {
+        //agregar funcionalidad aqui
+        console.log('Evento Submit ok!');
+        
+    });
+
 
